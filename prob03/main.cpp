cc633abbfdcac682608d4de300f316f960ccc592
ALSumarinas <alsumarinas@cs104u-4c.acad.fullerton.edu>
2019-09-07 14:05:31 -0700

    Prob03-AndrewSumarinas

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,25 @@
 // This program calculates a person's height in feet (') and inches (").
+
+
+#include <iostream>
+
+int main()
+{
+  int myHeight1;
+  double myHeight2;
+  double myHeight3;
+
+  std::cout << "Please enter the person's height in inches: " << std::endl;
+  std::cin >> myHeight1;
+
+  myHeight2 = myHeight1 / 12;
+  myHeight3 = myHeight1 % 12;
+
+  std::cout << "That person is " << myHeight2 << "\'" << myHeight3  << "\""<< std::endl;
+
+  return 0;
+
+
+
+
+}
